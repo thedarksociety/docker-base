624844e3c36b0bdfee7b51d27586e6ee955c5f44
Rye Miller <millerrye17@gmail.com>
2020-12-01 21:02:12 -0700

    #5: Addition of build arg variables into the shell script.

--- build.sh
+++ build.sh
@@ -10,24 +10,49 @@
 
 set -eo pipefail
 
-# Set some script variables.
+# Set some script variables and run some checks
 # --------------------------------------------------------------------------- #
-if [ -z ${REPOSITORY_NAME:-} ] || [ -z ${PROJECT_ID:-} ]; then
-   echo "There is NO value set for REPOSITORY_NAME/PROJECT_ID."
-fi
+BUILD_DATE=$(date +"%Y-%m-%d::%H:%M:%S")
+BUILD_PATH=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
+REPO_NAME=$(pwd | awk -F '/' '{print $NF}')
 
+ALPINE="${BUILD_PATH}/image/alpine"
+DEBIAN="${BUILD_PATH}/image/debian"
+PYTHON="${BUILD_PATH}/image/python"
+
+IMAGE=$(echo "${BUILD_PATH}" | cut -d "-" -f 2)
+
+REF=$(git rev-parse --short HEAD)
+
+# --cache-from
 if [ ${NO_CACHE:-} = 'true' ]; then
-    echo "This will built w/o cache"
+    CACHE="--no-cache ."
+else
+    CACHE="."
 fi
 
-if [ -z ${DOCKERFILE:-} ]; then
-    echo "There is NO value set for the GIT_REF"
+echo $REGISTRY_NAME
+echo $PROJECT_ID
+echo $BUILD_PATH
+echo $REPOSITORY_NAME
+echo $REPO_NAME
+echo $IMAGE
+echo $BUILD_DATE
+echo $ALPINE
+echo $DEBIAN
+echo $PYTHON
+echo $REF
+echo $GIT_SHA
+echo $GIT_REF
+echo $CACHE
+
+#docker build \
+#    --build-arg DATE=${DATE} \
+#    --build-arg GIT_SHA=${GIT_SHA} \
+#    -t ${REGISTRY_NAME}:
+
+if [ ${RELEASE:-} = 'false' ]; then
+    echo "No images are pushed, this is only to build and test."
+else
+    echo "Preparing to push builds."
 fi
-
-TAG=1.2
-
-printf "\n\nSet Outputs\n"
-(
-    echo "::set-output name=name::this-is-a-success"
-    echo "::set-output name=version::${TAG}"
-)
